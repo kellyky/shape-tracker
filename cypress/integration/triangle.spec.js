807709857ca268137dd8a34cbfdbf6782f414e1d
Kelly Young <kkristineyoung@gmail.com>
2021-04-26 00:39:18 -0500

    Adds webpack configuration, cypress, starter files for triangle, also webpack, lint, cypress installed

---
+++ cypress/integration/triangle.spec.js
@@ -0,0 +1,12 @@
+import Triangle from '../../src/triangle.js';
+
+describe('Triangle', () => {
+  it('Does not do much!'), () => {
+    expect(true).to.equal(true)
+  });
+})
+
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
